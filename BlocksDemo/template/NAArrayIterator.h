8ef9a4fe6d9385c0f487d7ee698acc5d45e73764
Nico Ameghino <nicolas.ameghino@gmail.com>
2014-03-25 19:25:46 -0300

    Added the NAArrayIterator protocol

---
+++ BlocksDemo/template/NAArrayIterator.h
@@ -0,0 +1,15 @@
+//
+//  NAArrayIterator.h
+//  BlocksDemo
+//
+//  Created by Nicolas Ameghino on 3/25/14.
+//  Copyright (c) 2014 Nicolas Ameghino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NAArrayIterator <NSObject>
+
+-(void) runOnArray:(NSArray*) array;
+
+@end
