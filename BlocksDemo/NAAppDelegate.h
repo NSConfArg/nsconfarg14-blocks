c6d4d474fbbaf96f7affee692226bd1581ac9d3f
Nico Ameghino <nicolas.ameghino@gmail.com>
2014-03-25 17:34:13 -0300

    Initial Commit

---
+++ BlocksDemo/NAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NAAppDelegate.h
+//  BlocksDemo
+//
+//  Created by Nicolas Ameghino on 3/25/14.
+//  Copyright (c) 2014 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlocksDemoTests/BlocksDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  BlocksDemoTests.m
+//  BlocksDemoTests
+//
+//  Created by Nicolas Ameghino on 3/25/14.
+//  Copyright (c) 2014 Nicolas Ameghino. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlocksDemoTests : XCTestCase
+
+@end
+
+@implementation BlocksDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
