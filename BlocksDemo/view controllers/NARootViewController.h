f6545437341e3e71c2f7ff43490a6d0c0dfa679b
Nico Ameghino <nicolas.ameghino@gmail.com>
2014-03-25 19:24:53 -0300

    Final view controller example for templates

---
+++ BlocksDemo/view controllers/NARootViewController.h
@@ -0,0 +1,13 @@
+//
+//  NARootViewController.h
+//  BlocksDemo
+//
+//  Created by Nicolas Ameghino on 3/25/14.
+//  Copyright (c) 2014 Nicolas Ameghino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NARootViewController : UIViewController
+
+@end
---
+++ BlocksDemo/view controllers/NARootViewController.m
@@ -0,0 +1,72 @@
+//
+//  NARootViewController.m
+//  BlocksDemo
+//
+//  Created by Nicolas Ameghino on 3/25/14.
+//  Copyright (c) 2014 Nicolas Ameghino. All rights reserved.
+//
+
+#import "NARootViewController.h"
+#import "NASpeakersArrayIterator.h"
+#import "NATopicsArrayIterator.h"
+#import "NABlockyArrayIterator.h"
+
+@interface NARootViewController ()
+
+@property(nonatomic, strong) UITextView* textView;
+
+@property(nonatomic, strong) NSArray *speakers;
+@property(nonatomic, strong) NSArray *topics;
+
+@end
+
+@implementation NARootViewController
+
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        _speakers = @[@"Julio Carretoni", @"Ezequiel Aceto", @"Gonzalo Larralde", @"Nicolas Ameghino"];
+        _topics = @[@"Performance", @"iBeacons", @"Peer to peer", @"Blocks"];
+    }
+    return self;
+}
+
+-(void)loadView {
+    [super loadView];
+    _textView = [[UITextView alloc] initWithFrame:self.view.bounds];
+    _textView.editable = NO;
+    [self.view addSubview:_textView];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    self.textView.text = @"ready\n";
+    
+    [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                     target:self
+                                   selector:@selector(run:)
+                                   userInfo:nil
+                                    repeats:NO];
+}
+
+-(void) run:(id) sender {
+    NATemplateArrayIterator *speakersArrayIterator = [[NASpeakersArrayIterator alloc] init];
+    speakersArrayIterator.textView = self.textView;
+    [speakersArrayIterator runOnArray:self.speakers];
+    
+    NATemplateArrayIterator *topicsArrayIterator = [[NATopicsArrayIterator alloc] init];
+    topicsArrayIterator.textView = self.textView;
+    [topicsArrayIterator runOnArray:self.topics];
+    
+    NABlockyArrayIterator *speakersBlockyArrayIterator = [[NABlockyArrayIterator alloc] init];
+
+    
+    __weak NARootViewController* weakSelf = self;
+    speakersBlockyArrayIterator.doEachBlock = ^(id o) {
+        weakSelf.textView.text = [weakSelf.textView.text stringByAppendingFormat:@"%@\n", o];
+    };
+    
+    [speakersBlockyArrayIterator runOnArray:self.speakers];
+    
+}
+
+@end
