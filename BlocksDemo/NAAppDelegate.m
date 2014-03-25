482afe7de34f252dccbf8d94ac5f00faa6b1e868
Nico Ameghino <nicolas.ameghino@gmail.com>
2014-03-25 19:25:58 -0300

    Initial implementation

--- BlocksDemo/NAAppDelegate.m
+++ BlocksDemo/NAAppDelegate.m
@@ -7,14 +7,19 @@
 //
 
 #import "NAAppDelegate.h"
-
+#import "NARootViewController.h"
 @implementation NAAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    UIViewController *rootViewController = [[NARootViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc]
+                                                    initWithRootViewController:rootViewController];
+    navigationController.navigationBarHidden = YES;
+    self.window.rootViewController = navigationController;
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -27,7 +32,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
